f0a896b6e570d55eb9cd276948ffa6cc7b2eda29
Masaaki Shibata <mshibata@emptypage.jp>
2019-11-15 22:21:42 +0900

    Remove redundant parentheses. Thanks to PanderMusubi. ref. https://bitbucket.org/emptypage/uniseg-python/pull-requests/3/added-relevant-urls-and-removed-redundant/diff

--- uniseg/wordbreak.py
+++ uniseg/wordbreak.py
@@ -52,7 +52,7 @@ Numeric_MidNumLet_FormatFE = 'Numeric_MidNumLet_FormatFE'
 break_table_index = [
     Other,
     CR,
-    LF, 
+    LF,
     Newline,
     Katakana,
     ALetter,
@@ -106,33 +106,33 @@ break_table = [
 
 
 def word_break(c, index=0):
-    
+
     r"""Return the Word_Break property of `c`
-    
+
     `c` must be a single Unicode code point string.
-    
+
     >>> print(word_break('\x0d'))
     CR
     >>> print(word_break('\x0b'))
     Newline
     >>> print(word_break('\u30a2'))
     Katakana
-    
-    If `index` is specified, this function consider `c` as a unicode 
-    string and return Word_Break property of the code point at 
+
+    If `index` is specified, this function consider `c` as a unicode
+    string and return Word_Break property of the code point at
     c[index].
-    
+
     >>> print(word_break('A\u30a2', 1))
     Katakana
     """
-    
+
     return _word_break(code_point(c, index))
 
 
 def _preprocess_boundaries(s):
-    
+
     r"""(internal) Preprocess WB4; X [Extend Format]* -> X
-    
+
     >>> result = list(_preprocess_boundaries('\r\n'))
     >>> result == [(0, 'CR'), (1, 'LF')]
     True
@@ -146,7 +146,7 @@ def _preprocess_boundaries(s):
     >>> result == [(0, 'Other'), (2, 'Other')]
     True
     """
-    
+
     prev_prop = None
     i = 0
     for c in code_points(s):
@@ -165,12 +165,12 @@ def _preprocess_boundaries(s):
 
 
 def word_breakables(s):
-    
+
     r"""Iterate word breaking opportunities for every position of `s`
-    
-    1 for "break" and 0 for "do not break".  The length of iteration 
+
+    1 for "break" and 0 for "do not break".  The length of iteration
     will be the same as ``len(s)``.
-    
+
     >>> list(word_breakables(u'ABC'))
     [1, 0, 0]
     >>> list(word_breakables(u'Hello, world.'))
@@ -178,10 +178,10 @@ def word_breakables(s):
     >>> list(word_breakables(u'\x01\u0308\x01'))
     [1, 0, 1]
     """
-    
+
     if not s:
         return
-    
+
     primitive_boundaries = list(_preprocess_boundaries(s))
     prev_prev_wb = None
     prev_wb = None
@@ -225,7 +225,7 @@ def word_breakables(s):
               or (prev_wb in (ALetter, Numeric, Katakana, ExtendNumLet)
                   and wb == ExtendNumLet)
               or (prev_wb == ExtendNumLet
-                  and wb in ((ALetter, Numeric, Katakana)))
+                  and wb in (ALetter, Numeric, Katakana))
               ):
             do_break = False
         # WB13c.
@@ -242,13 +242,13 @@ def word_breakables(s):
 
 
 def word_boundaries(s, tailor=None):
-    
+
     """Iterate indices of the word boundaries of `s`
-    
-    This function yields indices from the first boundary position (> 0) 
+
+    This function yields indices from the first boundary position (> 0)
     to the end of the string (== len(s)).
     """
-    
+
     breakables = word_breakables(s)
     if tailor is not None:
         breakables = tailor(s, breakables)
@@ -256,19 +256,19 @@ def word_boundaries(s, tailor=None):
 
 
 def words(s, tailor=None):
-    
+
     """Iterate *user-perceived* words of `s`
-    
+
     These examples bellow is from
     http://www.unicode.org/reports/tr29/tr29-15.html#Word_Boundaries
-    
+
     >>> s = 'The quick (“brown”) fox can’t jump 32.3 feet, right?'
     >>> print('|'.join(words(s)))
     The| |quick| |(|“|brown|”|)| |fox| |can’t| |jump| |32.3| |feet|,| |right|?
     >>> list(words(u''))
     []
     """
-    
+
     breakables = word_breakables(s)
     if tailor is not None:
         breakables = tailor(s, breakables)
