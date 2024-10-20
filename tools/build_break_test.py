80b07884f5f30b6dfa4e7a9cddfb28499a2f6fb0
Masaaki Shibata <mshibata@emptypage.jp>
2024-10-21 04:37:28 +0900

    add test code generator script

---
+++ tools/build_break_test.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+"""Generate uniseg break test code."""
+
+import csv
+from argparse import ArgumentParser, FileType
+from textwrap import wrap
+from typing import NamedTuple, TextIO
+
+
+class TestEntry(NamedTuple):
+    """Data class for test record."""
+    name: str       # 'XxxxBreakTestNNNN'
+    pattern: str    # '÷ 0020 ÷ 0020 ÷'
+    comment: str    # '÷ [0.2] SPACE (Other) ÷ [999.0] SPACE (Other) ÷ [0.3]
+
+
+def parse_breaking_test_pattern(pattern: str) -> tuple[str, list[int]]:
+    """Parse `pattern` and return test string and expected result."""
+
+    BREAK = '\u00f7'
+    DONT_BREAK = '\u00d7'
+    codepoints: list[str] = []
+    breakpoints: list[int] = []
+    index = 0
+    for token in pattern.split():
+        if token == BREAK:
+            breakpoints.append(index)
+        elif token == DONT_BREAK:
+            pass
+        else:
+            c = chr(int(token, 16))
+            codepoints.append(c)
+            index += len(c)
+    return ''.join(codepoints), breakpoints
+
+
+def generate_break_test_code(test: TestEntry, break_func_name: str) -> str:
+    """Return test function code."""
+
+    string, expect = parse_breaking_test_pattern(test.pattern)
+    string_literal = string.encode('unicode_escape').decode('ascii')
+    doc_string = '\n    '.join(wrap(test.comment, 76))
+
+    return (
+        f'def test_{test.name.lower()}() -> None:\n'
+        f'    """{test.pattern}\n'
+        f'\n'
+        f'    {doc_string}\n'
+        f'    """\n'
+        f'    # {string}\n'
+        f'    actual = list({break_func_name}(\'{string_literal}\'))\n'
+        f'    expect = {repr(expect)}\n'
+        f'    assert expect == actual\n'
+    )
+
+
+def main() -> None:
+    """Main entry point."""
+
+    parser = ArgumentParser()
+    parser.add_argument('-m', '--module', default='',
+                        help='module name for importing break_func')
+    parser.add_argument('-o', '--output', default='-',
+                        type=FileType('w', encoding='utf-8'))
+    parser.add_argument('break_func')
+    parser.add_argument('input', type=FileType('r', encoding='utf-8'),
+                        help='test csv file')
+    args = parser.parse_args()
+
+    module_name: str = args.module
+    break_func: str = args.break_func
+    input: TextIO = args.input
+    output: TextIO = args.output
+
+    codes: list[str] = [
+        '#!/usr/bin/env python3\n'
+        '# DO NOT EDIT.  This code is generated automatically.\n'
+    ]
+
+    if module_name:
+        codes.append(f'from {module_name} import {break_func}\n')
+
+    for fields in csv.reader(input):
+        test = TestEntry(*fields)
+        codes.append(generate_break_test_code(test, break_func))
+
+    output.write('\n\n'.join(codes))
+
+
+if __name__ == '__main__':
+    main()
