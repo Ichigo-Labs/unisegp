f9f9605621aa83680005da67e7fcfc4d7eda2d82
Masaaki Shibata <mshibata@emptypage.jp>
2024-10-22 01:13:16 +0900

    format code

--- src/uniseg/codepoint.py
+++ src/uniseg/codepoint.py
@@ -1,19 +1,19 @@
-"""Unicode code point. """
+"""Unicode code point."""
 
-from builtins import ord as _ord, chr as _chr
-from typing import Optional, Iterator
-
+from builtins import chr as _chr
+from builtins import ord as _ord
+from typing import Iterator, Optional
 
 __all__ = [
     'ord',
     'chr',
-    'code_points'
+    'code_point',
+    'code_points',
 ]
 
 
 def ord(c: str, index: Optional[int] = None, /) -> int:
-
-    """Return the integer value of the Unicode code point `c`
+    """Return the integer value of the Unicode code point `c`.
 
     >>> ord('a')
     97
@@ -34,7 +34,7 @@ def ord(c: str, index: Optional[int] = None, /) -> int:
 
     When `index` argument is specified (to not ``None``), this function
     treats `c` as a Unicode string and returns integer value of code
-    point at ``c[index]`` (or may be ``c[index:index+2]``):
+    point at ``c[index]``:
 
     >>> ord('hello', 0)
     104
@@ -43,26 +43,22 @@ def ord(c: str, index: Optional[int] = None, /) -> int:
     >>> ord('a\\U00020b9f', 1)
     134047
     """
-
     return _ord(c if index is None else c[index])
 
 
 def chr(cp: int, /) -> str:
-
-    """Return the unicode object represents the code point integer `cp`
+    """Return the single `str` object which represents the integer code point.
 
     >>> chr(0x61) == 'a'
     True
     >>> chr(0x20b9f) == '\\U00020b9f'
     True
     """
-
     return _chr(cp)
 
 
 def code_point(s: str, index: int = 0, /) -> str:
-
-    """Return code point at s[index]
+    """Return code point at s[index].
 
     >>> code_point('ABC') == 'A'
     True
@@ -73,13 +69,11 @@ def code_point(s: str, index: int = 0, /) -> str:
     >>> code_point('\\U00020b9f\\u3042', 1) == '\\u3042'
     True
     """
-
     return s[index]
 
 
 def code_points(s: str, /) -> Iterator[str]:
-
-    """Iterate every Unicode code points of the unicode string `s`
+    """Iterate every Unicode code points of the unicode string `s`.
 
     >>> s = 'hello'
     >>> list(code_points(s)) == ['h', 'e', 'l', 'l', 'o']
@@ -88,7 +82,6 @@ def code_points(s: str, /) -> Iterator[str]:
     >>> list(code_points(s)) == ['a', 'b', 'c', '\\U00020b9f', '\\u3042']
     True
     """
-
     return iter(s)
 
 
