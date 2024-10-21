08bff8bcddf707abc5e34d6ae4967e61bea9cc6e
Masaaki Shibata <mshibata@emptypage.jp>
2024-10-22 01:33:16 +0900

    format code

--- src/uniseg/db.py
+++ src/uniseg/db.py
@@ -1,33 +1,34 @@
 """uniseg database lookup interface. """
 
-from uniseg import db_lookups
+from sys import maxunicode
+
 from uniseg.codepoint import ord
+from uniseg.db_lookups import (grapheme_cluster_break_list, index1, index2,
+                               line_break_list, sentence_break_list, shift,
+                               word_break_list)
 
 
-def _find_break(u: str, /) -> int:
-    """
-    find code code point in hashmap
-    """
-    code = ord(u)
-    if code >= 0x110000:
+def _find_break(ch: str, /) -> int:
+    """Find code point in hashmap."""
+    cp = ord(ch)
+    if maxunicode < cp:
         return 0
     else:
-        index = db_lookups.index1[code >> db_lookups.shift]
-        return db_lookups.index2[
-            (index << db_lookups.shift) + (code & ((1 << db_lookups.shift) - 1))
-        ]
-
-def grapheme_cluster_break(u: str, /) -> str:
-    return db_lookups.grapheme_cluster_break_list[_find_break(u)]
+        index = index1[cp >> shift]
+        return index2[(index << shift) + (cp & ((1 << shift) - 1))]
 
 
-def word_break(u: str, /) -> str:
-    return db_lookups.word_break_list[_find_break(u)]
+def grapheme_cluster_break(ch: str, /) -> str:
+    return grapheme_cluster_break_list[_find_break(ch)]
 
 
-def sentence_break(u: str, /) -> str:
-    return db_lookups.sentence_break_list[_find_break(u)]
+def word_break(ch: str, /) -> str:
+    return word_break_list[_find_break(ch)]
 
 
-def line_break(u: str, /) -> str:
-    return db_lookups.line_break_list[_find_break(u)]
+def sentence_break(ch: str, /) -> str:
+    return sentence_break_list[_find_break(ch)]
+
+
+def line_break(ch: str, /) -> str:
+    return line_break_list[_find_break(ch)]
