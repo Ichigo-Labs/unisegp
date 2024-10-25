d9e60a2fdb804324be2d3c34d79266a09ead4167
Masaaki Shibata <mshibata@emptypage.jp>
2024-10-25 20:24:06 +0900

    refactor with iter_code_point_properties

--- tools/prop2csv.py
+++ tools/prop2csv.py
@@ -4,7 +4,7 @@
 import csv
 from argparse import ArgumentParser, FileType
 
-from uniseg.ucdtools import CodePointSpan, iter_records
+from uniseg.ucdtools import iter_code_point_properties
 
 
 def main() -> None:
@@ -18,15 +18,7 @@ def main() -> None:
                         help='UCD test file')
     args = parser.parse_args()
 
-    rows = []
-    for record in iter_records(args.file):
-        fields, __ = record
-        if len(fields) > 1:
-            span = CodePointSpan(fields[0])
-            props = fields[1:]
-            for cp in span:
-                rows.append((cp,) + props)
-    rows.sort()
+    rows = sorted(iter_code_point_properties(args.file))
     writer = csv.writer(args.output)
     writer.writerows(rows)
 
