6ed572b3d04c259deeff99837501850f6e2aca84
Masaaki Shibata <mshibata@emptypage.jp>
2024-10-27 01:02:33 +0900

    [Refactoring] format code

--- src/uniseg/sentencebreak.py
+++ src/uniseg/sentencebreak.py
@@ -1,16 +1,16 @@
 """Unicode sentence boundaries.
 
-UAX #29: Unicode Text Segmentation (Unicode 15.0.0)
-https://www.unicode.org/reports/tr29/tr29-41.html
+UAX #29: Unicode Text Segmentation (Unicode 16.0.0)
+https://www.unicode.org/reports/tr29/tr29-45.html
 """
 
 from enum import Enum
-from typing import Iterator, Optional, Tuple, Sequence, List
+from typing import Iterator, List, Optional, Sequence, Tuple
 
-from uniseg.breaking import boundaries, break_units, Breakables, TailorFunc
+from uniseg.breaking import (Breakables, Runner, TailorFunc, boundaries,
+                             break_units)
 from uniseg.codepoint import code_point, code_points
 from uniseg.db import sentence_break as _sentence_break
-
 
 __all__ = [
     'SentenceBreak',
@@ -23,8 +23,7 @@ __all__ = [
 
 
 class SentenceBreak(Enum):
-    """Sentence_Break property values. """
-
+    """Sentence_Break property values."""
     OTHER = 'Other'
     CR = 'CR'
     LF = 'LF'
@@ -47,8 +46,7 @@ SB = SentenceBreak
 
 
 def sentence_break(c: str, index: int = 0, /) -> SentenceBreak:
-
-    r"""Return Sentence_Break property value of `c`
+    R"""Return Sentence_Break property value of `c`.
 
     `c` must be a single Unicode code point string.
 
@@ -69,13 +67,12 @@ def sentence_break(c: str, index: int = 0, /) -> SentenceBreak:
     >>> sentence_break('/')
     <SentenceBreak.OTHER: 'Other'>
     """
-
     return SentenceBreak[_sentence_break(code_point(c, index)).upper()]
 
 
 def _preprocess_boundaries(s: str, /) -> Iterator[Tuple[int, SentenceBreak]]:
 
-    r"""(internal)
+    R"""(internal)
 
     >>> from pprint import pprint
     >>> pprint(list(_preprocess_boundaries('Aa')),
@@ -98,7 +95,6 @@ def _preprocess_boundaries(s: str, /) -> Iterator[Tuple[int, SentenceBreak]]:
     [(0, <SentenceBreak.CR: 'CR'>), (1, <SentenceBreak.CR: 'CR'>),
      (2, <SentenceBreak.UPPER: 'Upper'>)]
     """
-
     prev_prop = None
     i = 0
     for c in code_points(s):
@@ -118,11 +114,10 @@ def _preprocess_boundaries(s: str, /) -> Iterator[Tuple[int, SentenceBreak]]:
 
 def _next_break(primitive_boundaries: Sequence[Tuple[int, SentenceBreak]],
                 pos: int,
-                expects: Sequence[SentenceBreak], /) -> Optional[SentenceBreak]:
-
-    """(internal)
-    """
-
+                expects: Sequence[SentenceBreak],
+                /
+                ) -> Optional[SentenceBreak]:
+    """(internal)"""
     for i in range(pos, len(primitive_boundaries)):
         sb = primitive_boundaries[i][1]
         if sb in expects:
@@ -131,9 +126,8 @@ def _next_break(primitive_boundaries: Sequence[Tuple[int, SentenceBreak]],
 
 
 def sentence_breakables(s: str, /) -> Breakables:
-
-    r"""Iterate sentence breaking opportunities for every position of
-    `s`
+    R"""Iterate sentence breaking opportunities for every position of
+    `s`.
 
     1 for "break" and 0 for "do not break".  The length of iteration
     will be the same as ``len(s)``.
@@ -145,7 +139,6 @@ def sentence_breakables(s: str, /) -> Breakables:
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
      0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     """
-
     primitive_boundaries = list(_preprocess_boundaries(s))
     prev_prev_prev_prev_sb = None
     prev_prev_prev_sb = None
@@ -154,7 +147,7 @@ def sentence_breakables(s: str, /) -> Breakables:
     pos = 0
     for i, (pos, sb) in enumerate(primitive_boundaries):
         next_pos, next_sb = (primitive_boundaries[i+1]
-                             if i<len(primitive_boundaries)-1 else (len(s), None))
+                             if i < len(primitive_boundaries)-1 else (len(s), None))
         if pos == 0:
             do_break = True
         # SB3
@@ -180,7 +173,7 @@ def sentence_breakables(s: str, /) -> Breakables:
                                SB.LF, SB.STERM, SB.ATERM]) == SB.LOWER):
             do_break = False
         # SB8a
-        elif ( (
+        elif ((
             (prev_sb in (SB.STERM, SB.ATERM))
             or (prev_prev_sb in (SB.STERM, SB.ATERM) and prev_sb == SB.CLOSE)
             or (prev_prev_sb in (SB.STERM, SB.ATERM) and prev_sb == SB.SP)
@@ -188,7 +181,7 @@ def sentence_breakables(s: str, /) -> Breakables:
                 and prev_prev_sb == SB.CLOSE and prev_sb == SB.SP)
         ) and (
             sb in (SB.SCONTINUE, SB.STERM, SB.ATERM)
-        ) ):
+        )):
             do_break = False
         # SB9
         elif (((prev_sb in (SB.STERM, SB.ATERM))
@@ -206,30 +199,30 @@ def sentence_breakables(s: str, /) -> Breakables:
             do_break = False
         # SB11
         elif ((prev_sb in (SB.STERM, SB.ATERM))
-               or (prev_prev_sb in (SB.STERM, SB.ATERM)
-                   and prev_sb == SB.CLOSE)
-               or (prev_prev_sb in (SB.STERM, SB.ATERM)
-                   and prev_sb == SB.SP)
-               or (prev_prev_sb in (SB.STERM, SB.ATERM)
-                   and prev_sb in (SB.SEP, SB.CR, SB.LF))
-               or (prev_prev_prev_sb in (SB.STERM, SB.ATERM)
-                   and prev_prev_sb == SB.CLOSE
-                   and prev_sb == SB.SP)
-               or (prev_prev_prev_sb in (SB.STERM, SB.ATERM)
-                   and prev_prev_sb == SB.CLOSE
-                   and prev_sb in (SB.SEP, SB.CR, SB.LF))
-               or (prev_prev_prev_sb in (SB.STERM, SB.ATERM)
-                   and prev_prev_sb == SB.SP
-                   and prev_sb in (SB.SEP, SB.CR, SB.LF))
-               or (prev_prev_prev_prev_sb in (SB.STERM, SB.ATERM)
-                   and prev_prev_prev_sb == SB.CLOSE
-                   and prev_prev_sb == SB.SP
-                   and prev_sb in (SB.SEP, SB.CR, SB.LF))):
+              or (prev_prev_sb in (SB.STERM, SB.ATERM)
+                  and prev_sb == SB.CLOSE)
+              or (prev_prev_sb in (SB.STERM, SB.ATERM)
+                  and prev_sb == SB.SP)
+              or (prev_prev_sb in (SB.STERM, SB.ATERM)
+                  and prev_sb in (SB.SEP, SB.CR, SB.LF))
+              or (prev_prev_prev_sb in (SB.STERM, SB.ATERM)
+                  and prev_prev_sb == SB.CLOSE
+                  and prev_sb == SB.SP)
+              or (prev_prev_prev_sb in (SB.STERM, SB.ATERM)
+                  and prev_prev_sb == SB.CLOSE
+                  and prev_sb in (SB.SEP, SB.CR, SB.LF))
+              or (prev_prev_prev_sb in (SB.STERM, SB.ATERM)
+                  and prev_prev_sb == SB.SP
+                  and prev_sb in (SB.SEP, SB.CR, SB.LF))
+              or (prev_prev_prev_prev_sb in (SB.STERM, SB.ATERM)
+                  and prev_prev_prev_sb == SB.CLOSE
+                  and prev_prev_sb == SB.SP
+                  and prev_sb in (SB.SEP, SB.CR, SB.LF))):
             do_break = True
         else:
             do_break = False
         for j in range(next_pos-pos):
-            yield 1 if j==0 and do_break else 0
+            yield 1 if j == 0 and do_break else 0
         prev_prev_prev_prev_sb = prev_prev_prev_sb
         prev_prev_prev_sb = prev_prev_sb
         prev_prev_sb = prev_sb
@@ -238,8 +231,7 @@ def sentence_breakables(s: str, /) -> Breakables:
 
 
 def sentence_boundaries(s: str, tailor: Optional[TailorFunc] = None, /) -> Iterator[int]:
-
-    r"""Iterate indices of the sentence boundaries of `s`
+    R"""Iterate indices of the sentence boundaries of `s`.
 
     This function yields from 0 to the end of the string (== len(s)).
 
@@ -251,7 +243,6 @@ def sentence_boundaries(s: str, tailor: Optional[TailorFunc] = None, /) -> Itera
     >>> list(sentence_boundaries(''))
     []
     """
-
     breakables = sentence_breakables(s)
     if tailor is not None:
         breakables = tailor(s, breakables)
@@ -259,14 +250,12 @@ def sentence_boundaries(s: str, tailor: Optional[TailorFunc] = None, /) -> Itera
 
 
 def sentences(s: str, tailor: Optional[TailorFunc] = None, /) -> Iterator[str]:
-
-    r"""Iterate every sentence of `s`
+    R"""Iterate every sentence of `s`.
 
     >>> s = 'He said, \u201cAre you going?\u201d John shook his head.'
     >>> list(sentences(s)) == ['He said, \u201cAre you going?\u201d ', 'John shook his head.']
     True
     """
-
     breakables = sentence_breakables(s)
     if tailor is not None:
         breakables = tailor(s, breakables)
