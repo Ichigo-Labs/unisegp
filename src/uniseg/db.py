5a13297b7acdad8c524243034d4accf7a5d00a58
Masaaki Shibata <mshibata@emptypage.jp>
2024-11-11 10:22:34 +0900

    [Refactoring]

--- src/uniseg/db.py
+++ src/uniseg/db.py
@@ -1,7 +1,5 @@
 """uniseg database lookup interface. """
 
-from sys import maxunicode
-
 from uniseg.db_lookups import columns, index1, index2, shift, values
 
 INDEX_GRAPHEME_CLUSTER_BREAK = columns.index('GraphemeClusterBreak')
@@ -12,35 +10,39 @@ INDEX_EXTENDED_PICTOGRAPHIC = columns.index('Extended_Pictographic')
 INDEX_INDIC_CONJUNCT_BREAK = columns.index('InCB')
 
 
-def _find_break(ch: str, /) -> int:
-    """Find code point in hashmap."""
-    cp = ord(ch)
-    if maxunicode < cp:
-        return 0
-    else:
-        index = index1[cp >> shift]
-        return index2[(index << shift) + (cp & ((1 << shift) - 1))]
+def get_value_index(key: int, /) -> int:
+    """Find index in hashmap."""
+    index = index1[key >> shift]
+    return index2[(index << shift) + (key & ((1 << shift) - 1))]
+
+
+def get_column_index(column_name: str) -> int:
+    return columns.index(column_name)
+
+
+def get_value(key: int, icol: int, /) -> str:
+    return values[get_value_index(key)][icol]
 
 
 def grapheme_cluster_break(ch: str, /) -> str:
-    return values[_find_break(ch)][INDEX_GRAPHEME_CLUSTER_BREAK] or 'Other'
+    return get_value(ord(ch), INDEX_GRAPHEME_CLUSTER_BREAK) or 'Other'
 
 
 def word_break(ch: str, /) -> str:
-    return values[_find_break(ch)][INDEX_WORD_BREAK] or 'Other'
+    return get_value(ord(ch), INDEX_WORD_BREAK) or 'Other'
 
 
 def sentence_break(ch: str, /) -> str:
-    return values[_find_break(ch)][INDEX_SENTENCE_BREAK] or 'Other'
+    return get_value(ord(ch), INDEX_SENTENCE_BREAK) or 'Other'
 
 
 def line_break(ch: str, /) -> str:
-    return values[_find_break(ch)][INDEX_LINE_BREAK] or 'XX'
+    return get_value(ord(ch), INDEX_LINE_BREAK) or 'XX'
 
 
 def extended_pictographic(ch: str, /) -> bool:
-    return bool(values[_find_break(ch)][INDEX_EXTENDED_PICTOGRAPHIC])
+    return bool(get_value(ord(ch), INDEX_EXTENDED_PICTOGRAPHIC))
 
 
 def indic_conjunct_break(ch: str, /) -> str:
-    return values[_find_break(ch)][INDEX_INDIC_CONJUNCT_BREAK] or 'None'
+    return get_value(ord(ch), INDEX_INDIC_CONJUNCT_BREAK) or 'None'
